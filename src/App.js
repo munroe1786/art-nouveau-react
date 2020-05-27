9523e8cedd2c6739b77ebc77f66a122c6cf819e7
munroe1786 <era3munroe@gmail.com>
2020-05-27 18:36:32 -0500

    delete component did mount function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch(`http://localhost:3000/api/v1/artists`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
   render() {
     return (
       <div className="App">
