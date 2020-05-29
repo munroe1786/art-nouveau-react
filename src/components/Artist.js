15a122698c79010f3cd35c849ed76758bc935db4
munroe1786 <era3munroe@gmail.com>
2020-05-29 17:59:25 -0500

    create WorksContainer class component / import WorksContainer into Artist.js and render it

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import {Redirect} from 'react-router-dom'
+import WorksContainer from '../containers/WorksContainer'
 
 const Artist = (props) => {
 
     let artist = props.artists[props.match.params.id - 1]
     console.log(artist)
     return (
-        <li>
+        <div>
+        <h2>
             {artist ? null : <Redirect to='/artists'/>}
             
             {artist ? artist.name : null}
-        </li>
+        </h2>
+        WorksContainer
+        </div>
     )
 
 }
