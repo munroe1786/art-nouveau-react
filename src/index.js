83cace19f53e32385fc3d3e57a508036ee00e5df
munroe1786 <era3munroe@gmail.com>
2020-05-29 07:34:49 -0500

    import BrowserRouter as Router and wrap App in Router tags

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import artistReducer from './reducers/artistReducer'
 
 import App from './App';
@@ -13,7 +14,9 @@ let store = createStore(artistReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>    
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
     document.getElementById('root'));
 
