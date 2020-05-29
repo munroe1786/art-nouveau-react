e7c9c1ffdbfab50b989654a7630185d59afc1094
munroe1786 <era3munroe@gmail.com>
2020-05-28 20:19:34 -0500

    add local state to ArtistInput to allow for controlled form---add values with object attributes to form fields

--- src/components/ArtistInput.js
+++ src/components/ArtistInput.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 class ArtistInput extends React.Component {
 
+    state = {name: '', date_of_birth: '', date_of_death: '', years_active: '', nationality: ''} //local state, inside component itself, not in redux store, might use global state if re-using forms--same for creation or updating
+
     //this component will have a form 
 
     render() {
@@ -10,23 +12,23 @@ class ArtistInput extends React.Component {
                 <form>
                     <p>
                     <label>Name</label>
-                    <input type='text' placeholder='Name'/>
+                    <input type='text' placeholder='Name' value={this.state.name}/>
                     </p>
                     <p>
                     <label>Born</label>
-                    <input type='text' placeholder='Born'/>
+                    <input type='text' placeholder='Born' value={this.state.date_of_birth}/>
                     </p>
                     <p>
                     <label>Died</label>
-                    <input type='text' placeholder='Died'/>
+                    <input type='text' placeholder='Died' value={this.state.date_of_death}/>
                     </p>
                     <p>
                     <label>Years Active</label>
-                    <input type='text' placeholder='Years Active'/>
+                    <input type='text' placeholder='Years Active' value={this.state.years_active}/>
                     </p>
                     <p>
                     <label>Nationality</label>
-                    <input type='text' placeholder='Nationality'/>
+                    <input type='text' placeholder='Nationality' value={this.state.nationality}/>
                     </p>
                 </form>
             </div>
