1c8f8f132ab25b612ec83a186e04b248a93b7d0b
munroe1786 <era3munroe@gmail.com>
2020-05-31 10:13:31 -0500

    add Works show props.works.map to Works.js

--- src/components/Works.js
+++ src/components/Works.js
@@ -4,7 +4,22 @@ const Works = (props) => {
 
     return (
         <div>
-            Works
+            {props.works && props.works.map(work =>
+               <li key={work.id}>
+                   <p>
+                   {work.title}
+                   </p>
+                   <p>
+                   {work.date}
+                   </p>
+                   <p>
+                       {work.image_url}
+                    </p>
+                   <p>
+                   {work.description}
+                   </p>
+               </li>    
+            )}
         </div>
     )
 }
