93218b5897405c034e09e3a99a39120b66757b3a
munroe1786 <era3munroe@gmail.com>
2020-05-31 10:43:18 -0500

    add form to WorkInput for new works

--- src/components/WorkInput.js
+++ src/components/WorkInput.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 class WorkInput extends React.Component {
 
     render() {
         return (
             <div>
-                WorkInput
+                <form>
+                    <p>
+                    <label>Title</label>
+                    <input type='text' placeholder='Title'name="name"></input>
+                    </p>
+                    <p>
+                    <label>Date</label>
+                    <input type='text' placeholder='Date'name="date"></input>
+                    </p>
+                    <p>
+                    <label>Image</label>
+                    <input type='text' placeholder='Image'name="image"></input>
+                    </p>
+                    <p>
+                    <label>Description</label>
+                    <input type='text' placeholder='Description'name="description"></input>
+                    </p>
+                </form>
             </div>
         )
     }
 }
 
-export default WorkInput
+export default connect(null, )(WorkInput)
+
+//connect this component so that when we call new transaction function, it dispatches function to our reducer.
+//receives props from TransactionContainer---but will need mapDispatch
