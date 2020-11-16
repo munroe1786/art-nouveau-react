4c73bce0b988c3c6c32c8f1dd615f199de102e05
munroe1786 <era3munroe@gmail.com>
2020-11-16 11:26:22 -0600

    add index.js to actions folder and export constants of the different actions

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export const ADD_ARTIST = "ADD_ARTIST"
+export const ADD_WORK = "ADD_WORK"
+export const DELETE_WORK = "DELETE_WORK"
+export const EDIT_ARTIST = "EDIT_ARTIST"
+export const FETCH_ARTIST = "FETCH_ARTIST"
+
+
