bd0ae8dca6d66763e4823aa7e3dba6c27d0a1935
munroe1786 <era3munroe@gmail.com>
2020-11-16 11:45:10 -0600

    add root reducer to index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer, 
+  composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>  
